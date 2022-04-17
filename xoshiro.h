830b8b89b6f6974eb024285133c206d3850e36cc
ellie timoney <flrl@users.noreply.github.com>
2022-04-18 08:32:18 +1000

    xoshiro: make xoshiro256plus usable

--- xoshiro.h
+++ xoshiro.h
@@ -57,4 +57,25 @@ extern uint32_t xoshiro128starstar_next(struct xoshiro128starstar_state *state);
 extern void xoshiro128starstar_jump(struct xoshiro128starstar_state *state);
 extern void xoshiro128starstar_long_jump(struct xoshiro128starstar_state *state);
 
+
+/* This is xoshiro256+ 1.0, our best and fastest generator for floating-point
+   numbers. We suggest to use its upper bits for floating-point
+   generation, as it is slightly faster than xoshiro256++/xoshiro256**. It
+   passes all tests we are aware of except for the lowest three bits,
+   which might fail linearity tests (and just those), so if low linear
+   complexity is not considered an issue (as it is usually the case) it
+   can be used to generate 64-bit outputs, too.
+
+   We suggest to use a sign test to extract a random Boolean value, and
+   right shifts to extract subsets of bits.
+
+   The state must be seeded so that it is not everywhere zero. If you have
+   a 64-bit seed, we suggest to seed a splitmix64 generator and use its
+   output to fill s. */
+struct xoshiro256plus_state {
+    uint64_t s[4];
+};
+extern uint64_t xoshiro256plus_next(struct xoshiro256plus_state *state);
+extern void xoshiro256plus_jump(struct xoshiro256plus_state *state);
+extern void xoshiro256plus_long_jump(struct xoshiro256plus_state *state);
 #endif
