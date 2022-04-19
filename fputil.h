30843a7b9992c16dfe4deadfa6345b0c3ae7ca75
ellie timoney <flrl@users.noreply.github.com>
2022-04-19 16:13:44 +1000

    add fputil

---
+++ fputil.h
@@ -0,0 +1,6 @@
+#ifndef LIBFLRL_FPUTIL_H
+#define LIBFLRL_FPUTIL_H
+
+extern int floats_equalish(float a, float b, float epsilon, float abs_th);
+
+#endif
---
+++ src/fputil.c
@@ -0,0 +1,26 @@
+#include <float.h>
+#include <math.h>
+
+#include "fputil.h"
+
+int floats_equalish(float a, float b, float epsilon, float abs_th)
+{
+    /* https://stackoverflow.com/a/32334103 */
+    float diff, norm, ref;
+
+    if (epsilon == 0.0f) epsilon = 128 * FLT_EPSILON;
+    if (abs_th == 0.0f) abs_th = FLT_MIN;
+
+    if (a == b) return 1;
+
+    diff = fabsf(a - b);
+    norm = fabsf(a + b);
+    if (norm > FLT_MAX) norm = FLT_MAX;
+
+    ref = epsilon * norm;
+    if (ref < abs_th) ref = abs_th;
+
+    if (diff < ref) return 1;
+
+    return 0;
+}
