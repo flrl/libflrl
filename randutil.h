e2230595899afbfdfdbc3dc33a22968a77573c07
ellie timoney <flrl@users.noreply.github.com>
2022-05-04 08:58:27 +1000

    randutil: weighted sample32 functions
    (sample64 equivalents sketched but not implemented yet)

--- randutil.h
+++ randutil.h
@@ -6,4 +6,36 @@ extern uint32_t rand32_inrange(uint32_t (*randfunc)(void *), void *randstate,
 
 extern uint64_t rand64_inrange(uint64_t (*randfunc)(void *), void *randstate,
                                uint64_t min, uint64_t max);
+
+struct weight {
+    uint16_t weight;
+    uint16_t cumulative;
+};
+
+/* n weights, build temp cdf, return index */
+extern unsigned sample32(uint32_t (*randfunc)(void *), void *randstate,
+                         const unsigned weights[], size_t n_weights);
+
+/* n weight|value pairs, build temp cdf, return value */
+extern unsigned sample32v(uint32_t (*randfunc)(void *), void *randstate,
+                          size_t n_pairs, ...);
+
+/* n elems of size z with struct weight at offset t, save cdf, return index */
+extern unsigned sample32p(uint32_t (*randfunc)(void *), void *randstate,
+                          void *data, size_t rows, size_t rowsize,
+                          size_t weight_offset);
+
+/* n weights, build temp cdf, return index */
+extern unsigned sample64(uint64_t (*randfunc)(void *), void *randstate,
+                         const unsigned weights[], size_t n_weights);
+
+/* n weight|value pairs, build temp cdf, return value */
+extern unsigned sample64v(uint64_t (*randfunc)(void *), void *randstate,
+                          size_t n_pairs, ...);
+
+/* n elems of size z with struct weight at offset t, save cdf, return index */
+extern unsigned sample64p(uint64_t (*randfunc)(void *), void *randstate,
+                          void *data, size_t rows, size_t rowsize,
+                          size_t weight_offset);
+
 #endif
--- src/randutil.c
+++ src/randutil.c
@@ -1,5 +1,7 @@
 #include <assert.h>
+#include <stdarg.h>
 #include <stdint.h>
+#include <stdlib.h>
 
 #include "randutil.h"
 
@@ -38,3 +40,142 @@ uint64_t rand64_inrange(uint64_t (*randfunc)(void *), void *randstate,
 
     return min + value % range;
 }
+
+unsigned sample32(uint32_t (*randfunc)(void *), void *randstate,
+                  const unsigned weights[], size_t n_weights)
+{
+    unsigned *cdf = NULL;
+    unsigned sum = 0;
+    uint32_t rand;
+    size_t i;
+
+    assert(n_weights > 0);
+    if (n_weights <= 1) return 0;
+
+    cdf = malloc(n_weights * sizeof(*cdf));
+    if (!cdf) return 0;
+
+    for (i = 0; i < n_weights; i++) {
+        sum += weights[i];
+        assert(i == 0 || sum >= cdf[i - 1]); /* overflow detection */
+        cdf[i] = sum;
+    }
+
+    assert(sum > 0);
+    if (sum == 0) return 0;
+
+    rand = rand32_inrange(randfunc, randstate, 0, sum - 1);
+    for (i = 0; i < n_weights && rand >= cdf[i]; i++)
+        ;
+
+    free(cdf);
+    return i;
+}
+
+unsigned sample32v(uint32_t (*randfunc)(void *), void *randstate,
+                   size_t n_pairs, ...)
+{
+    unsigned *values = NULL;
+    unsigned *cdf = NULL;
+    unsigned sum = 0;
+    unsigned ret;
+    uint32_t rand;
+    va_list ap;
+    size_t i;
+
+    assert(n_pairs > 0);
+    if (n_pairs == 0) return 0;
+
+    values = malloc(n_pairs * sizeof(*values));
+    cdf = malloc(n_pairs * sizeof(*cdf));
+    if (!values || !cdf) {
+        free(values);
+        free(cdf);
+        return 0;
+    }
+
+    va_start(ap, n_pairs);
+    for (i = 0; i < n_pairs; i++) {
+        unsigned weight = va_arg(ap, unsigned);
+        unsigned value = va_arg(ap, unsigned);
+        sum += weight;
+        assert(i == 0 || sum >= cdf[i - 1]); /* overflow detection */
+        values[i] = value;
+        cdf[i] = sum;
+    }
+    va_end(ap);
+
+    assert(sum > 0);
+    if (sum == 0) return 0;
+
+    rand = rand32_inrange(randfunc, randstate, 0, sum - 1);
+    for (i = 0; i < n_pairs && rand >= cdf[i]; i++)
+        ;
+
+    ret = values[i];
+    free(values);
+    free(cdf);
+
+    return ret;
+}
+
+/* n elems of size z with struct weight at offset t, save cdf, return index */
+unsigned sample32p(uint32_t (*randfunc)(void *), void *randstate,
+                   void *data, size_t rows, size_t rowsize,
+                   size_t weight_offset)
+{
+    void *p;
+    struct weight *wp, *prev;
+    uint16_t sum = 0;
+    uint32_t rand;
+    size_t i;
+
+    assert(rows > 0);
+    assert(rowsize > 0);
+
+    /* lazy load cdf when first cumulative value is zero */
+    wp = (struct weight *)(data + weight_offset);
+    if (wp->cumulative == 0) {
+        prev = NULL;
+        for (p = data; p < (data + rows * rowsize); p += rowsize) {
+            wp = (struct weight *)(p + weight_offset);
+            sum += wp->weight;
+            assert(prev == NULL || sum >= prev->cumulative); /* overflow */
+            wp->cumulative = sum;
+        }
+    }
+    assert(sum > 0);
+    if (sum == 0) return 0;
+
+    rand = rand32_inrange(randfunc, randstate, 0, sum - 1);
+
+    prev = NULL;
+    i = 0;
+    do {
+        wp = (struct weight *)((data + i * rowsize) + weight_offset);
+
+        if (rand < wp->cumulative
+            && (prev == NULL || rand >= prev->cumulative))
+        {
+            break;
+        }
+
+        i++;
+        prev = wp;
+    } while (i < rows);
+
+    return i;
+}
+
+/* n weights, build temp cdf, return index */
+extern unsigned sample64(uint64_t (*randfunc)(void *), void *randstate,
+                         const unsigned weights[], size_t n_weights);
+
+/* n weight|value pairs, build temp cdf, return value */
+extern unsigned sample64v(uint64_t (*randfunc)(void *), void *randstate,
+                          size_t n_pairs, ...);
+
+/* n elems of size z with struct weight at offset t, save cdf, return index */
+extern unsigned sample64p(uint64_t (*randfunc)(void *), void *randstate,
+                          void *data, size_t rows, size_t rowsize,
+                          size_t weight_offset);
