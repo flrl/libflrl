de027172fa65ce9ceb3950960d14238b049fd77c
ellie timoney <flrl@users.noreply.github.com>
2024-12-29 08:08:44 +1100

    fputil: add kahan babushka neumaier sums

--- flrl/fputil.h
+++ flrl/fputil.h
@@ -1,6 +1,11 @@
 #ifndef LIBFLRL_FPUTIL_H
 #define LIBFLRL_FPUTIL_H
 
+#include <stdlib.h>
+
 extern int floats_equalish(float a, float b, float epsilon, float abs_th);
+
+double kbn_sumf32v(const float *values, size_t n_values);
+double kbn_sumf64v(const double *values, size_t n_values);
 
 #endif
--- src/fputil.c
+++ src/fputil.c
@@ -24,3 +24,42 @@ int floats_equalish(float a, float b, float epsilon, float abs_th)
 
     return 0;
 }
+
+/* Kahan Babushka Neumaier sum */
+double kbn_sumf32v(const float *values, size_t n_values)
+{
+    double sum = 0.0, c = 0.0;
+    size_t i;
+
+    for (i = 0; i < n_values; i++) {
+        double t = sum + values[i];
+
+        if (fabs(sum) >= fabs(values[i]))
+            c += (sum - t) + values[i];
+        else
+            c += (values[i] - t) + sum;
+
+        sum = t;
+    }
+
+    return sum + c;
+}
+
+double kbn_sumf64v(const double *values, size_t n_values)
+{
+    double sum = 0.0, c = 0.0;
+    size_t i;
+
+    for (i = 0; i < n_values; i++) {
+        double t = sum + values[i];
+
+        if (fabs(sum) >= fabs(values[i]))
+            c += (sum - t) + values[i];
+        else
+            c += (values[i] - t) + sum;
+
+        sum = t;
+    }
+
+    return sum + c;
+}
