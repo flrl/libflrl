8fcc1b1660b84605a85cca7772270c800e97d36e
ellie timoney <flrl@users.noreply.github.com>
2025-01-02 09:58:35 +1100

    randutil: reorient api around randbs

--- flrl/randutil.h
+++ flrl/randutil.h
@@ -56,90 +56,90 @@ struct randbs {
 extern uint64_t randbs_bits(struct randbs *bs, unsigned want_bits);
 extern unsigned randbs_zeroes(struct randbs *bs, unsigned limit);
 
-extern void randi32v(const struct rng *rng,
+extern void randi32v(struct randbs *bs,
                      int32_t *out,
                      size_t count,
                      int32_t min,
                      int32_t max);
-extern void randi64v(const struct rng *rng,
+extern void randi64v(struct randbs *bs,
                      int64_t *out,
                      size_t count,
                      int64_t min,
                      int64_t max);
-extern void randu32v(const struct rng *rng,
+extern void randu32v(struct randbs *bs,
                      uint32_t *out,
                      size_t count,
                      uint32_t min,
                      uint32_t max);
-extern void randu64v(const struct rng *rng,
+extern void randu64v(struct randbs *bs,
                      uint64_t *out,
                      size_t count,
                      uint64_t min,
                      uint64_t max);
-extern void randf32v(const struct rng *rng,
+extern void randf32v(struct randbs *bs,
                      float *out,
                      size_t count,
                      double min,
                      double max);
-extern void randf64v(const struct rng *rng,
+extern void randf64v(struct randbs *bs,
                      double *out,
                      size_t count,
                      double min,
                      double max);
 
-inline int32_t randi32(const struct rng *rng, int32_t min, int32_t max)
+inline int32_t randi32(struct randbs *bs, int32_t min, int32_t max)
 {
     int32_t v;
-    randi32v(rng, &v, 1, min, max);
+    randi32v(bs, &v, 1, min, max);
     return v;
 }
 
-inline int64_t randi64(const struct rng *rng, int64_t min, int64_t max)
+inline int64_t randi64(struct randbs *bs, int64_t min, int64_t max)
 {
     int64_t v;
-    randi64v(rng, &v, 1, min, max);
+    randi64v(bs, &v, 1, min, max);
     return v;
 }
 
-inline uint32_t randu32(const struct rng *rng, uint32_t min, uint32_t max)
+inline uint32_t randu32(struct randbs *bs, uint32_t min, uint32_t max)
 {
     uint32_t v;
-    randu32v(rng, &v, 1, min, max);
+    randu32v(bs, &v, 1, min, max);
     return v;
 }
 
-inline uint64_t randu64(const struct rng *rng, uint64_t min, uint64_t max)
+inline uint64_t randu64(struct randbs *bs, uint64_t min, uint64_t max)
 {
     uint64_t v;
-    randu64v(rng, &v, 1, min, max);
+    randu64v(bs, &v, 1, min, max);
     return v;
 }
 
-inline float randf32(const struct rng *rng, double min, double max)
+inline float randf32(struct randbs *bs, double min, double max)
 {
     float v;
-    randf32v(rng, &v, 1, min, max);
+    randf32v(bs, &v, 1, min, max);
     return v;
 }
 
-inline double randf64(const struct rng *rng, double min, double max)
+inline double randf64(struct randbs *bs, double min, double max)
 {
     double v;
-    randf64v(rng, &v, 1, min, max);
+    randf64v(bs, &v, 1, min, max);
     return v;
 }
 
-extern void gaussf32v(const struct rng *rng,
+extern void gaussf32v(struct randbs *bs,
                       float *out,
                       size_t count,
                       double mean,
                       double stddev);
 
-extern float gaussf32(const struct rng *rng, double mean, double stddev);
+extern float gaussf32(struct randbs *bs, double mean, double stddev);
 
-inline bool coin(const struct rng *rng, float p_true)
+inline bool coin(struct randbs *bs, float p_true)
 {
-    return randf32(rng, 0.0, 1.0) <= p_true;
+    return randf32(bs, 0.0, 1.0) <= p_true;
 }
 
 /* XXX wrandx functions for struct wrng */
@@ -151,15 +151,15 @@ struct weight {
 };
 
 /* n weights, build temp cdf, return index */
-extern unsigned sample32(const struct rng *r,
+extern unsigned sample32(struct randbs *bs,
                          const unsigned weights[], size_t n_weights);
 
 /* n weight|value pairs, build temp cdf, return value */
-extern unsigned sample32v(const struct rng *r,
+extern unsigned sample32v(struct randbs *bs,
                           size_t n_pairs, ...);
 
 /* n elems of size z with struct weight at offset t, save cdf, return index */
-extern unsigned sample32p(const struct rng *r,
+extern unsigned sample32p(struct randbs *bs,
                           void *data, size_t rows, size_t rowsize,
                           size_t weight_offset);
 
