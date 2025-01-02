0af0ec0e783910562c41716f2eef19fe18ce55bd
ellie timoney <flrl@users.noreply.github.com>
2025-01-02 13:46:51 +1100

    misc/rand_float: update for randutil changes

--- misc/rand_float.c
+++ misc/rand_float.c
@@ -11,58 +11,6 @@
 #include <time.h>
 #include <unistd.h>
 
-uint32_t get_bit(const struct rng *rng)
-{
-    static uint32_t n_saved_bits = 0;
-    static uint32_t saved_bits;
-    uint32_t bit;
-
-    if (n_saved_bits == 0) {
-        saved_bits = rng->func(rng->state);
-        n_saved_bits = 32;
-    }
-
-    bit = saved_bits & 1;
-    saved_bits = saved_bits >> 1;
-    n_saved_bits --;
-
-    return bit;
-}
-
-float downey_randf(const struct rng *rng)
-{
-    int mant, exp, high_exp, low_exp;
-    union {
-        float f;
-        int32_t i;
-    } low, high, ans;
-
-    low.f = 0.0; // XXX min arg?
-    high.f = 1.0; // XXX max arg?
-
-    /* extract the exponent fields from low and high */
-    low_exp = (low.i >> 23) & 0xff;
-    high_exp = (high.i >> 23) & 0xff;
-
-    /* choose random bits and decrement exp until a 1 appears.
-     * start at high_exp - 1 to leave room to maybe +1 later. */
-    for (exp = high_exp - 1; exp > low_exp; exp--) {
-        if (get_bit(rng)) break;
-    }
-
-    /* choose a random 23-bit mantissa */
-    mant = rng->func(rng->state) & 0x7FFFFF;
-
-    /* if the mantissa is zero, half the time we should move to the next
-     * exponent range */
-    if (mant == 0 && get_bit(rng)) exp++;
-
-    /* combine the exponent and the mantissa */
-    ans.i = (exp << 23) | mant;
-
-    return ans.f;
-}
-
 void count_in_range(float low, float high)
 {
     float x;
@@ -126,7 +74,6 @@ int main(int argc, char **argv)
 {
     int opt;
     bool do_counts = false;
-    bool do_downey = false;
     bool do_randutil = false;
     double low = 0.0, high = 1.0;
 
@@ -134,9 +81,6 @@ int main(int argc, char **argv)
         switch (opt) {
         case 'c':
             do_counts = true;
-            break;
-        case 'd':
-            do_downey = true;
             break;
         case 'h':
             high = atof(optarg);
@@ -161,27 +105,18 @@ int main(int argc, char **argv)
     }
 
 
-    if (do_downey || do_randutil) {
-        struct rng rng = RNG_INIT_XOSHIRO128_PLUSPLUS;
+    if (do_randutil) {
+        struct randbs bs = RANDBS_INITIALIZER(RNG_INIT_XOSHIRO128_PLUSPLUS);
         size_t n_values = 1 * 1000 * 1000;
         float *values;
         unsigned i;
 
-        assert(do_downey != do_randutil); /* XXX can't do both */
-
-        xoshiro128plusplus_seed64(rng.state, time(0));
+        randbs_seed64(&bs, time(NULL));
 
         values = calloc(n_values, sizeof(values[0]));
         if (!values) abort();
 
-        if (do_downey) {
-            for (i = 0; i < n_values; i++) {
-                values[i] = low + (high - low) * downey_randf(&rng);
-            }
-        }
-        else {
-            randf32v(&rng, values, n_values, low, high);
-        }
+        randf32v(&bs, values, n_values, low, high);
 
         for (i = 0; i < n_values; i++) {
             printf("%.*g,%.*g\n",
