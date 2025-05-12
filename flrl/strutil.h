3f0323d744f397d2153c83d2580e64fb8ec31369
ellie timoney <flrl@users.noreply.github.com>
2025-05-13 07:29:54 +1000

    strutil: add mnprintf and vmnprintf

---
+++ flrl/strutil.h
@@ -0,0 +1,24 @@
+#ifndef FLRL_STRUTIL_H
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/* XXX why is compiler insisting on "gnu_printf" for these? */
+__attribute__((format(gnu_printf, 2, 0)))
+extern char *vmnprintf(size_t maxlen, const char *restrict fmt, va_list args);
+
+__attribute__((format(gnu_printf, 2, 3)))
+inline char *mnprintf(size_t maxlen, const char *restrict fmt, ...)
+{
+    va_list args;
+    char *str = NULL;
+
+    va_start(args, fmt);
+    str = vmnprintf(maxlen, fmt, args);
+    va_end(args);
+
+    return str;
+}
+
+#endif
---
+++ src/strutil.c
@@ -0,0 +1,24 @@
+#include "flrl/strutil.h"
+
+#include <stdio.h>
+
+extern inline char *mnprintf(size_t maxlen, const char *restrict fmt, ...);
+
+char *vmnprintf(size_t maxlen, const char *restrict fmt, va_list args)
+{
+    va_list args_copy;
+    size_t len;
+    char *str = NULL;
+
+    va_copy(args_copy, args);
+    len = vsnprintf(NULL, 0, fmt, args_copy);
+    va_end(args_copy);
+
+    if (len > maxlen) len = maxlen;
+
+    str = malloc(len + 1);
+    if (!str) return NULL;
+
+    vsnprintf(str, len, fmt, args);
+    return str;
+}
