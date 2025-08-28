00e8203f9768bb1de41d797618e78262e4bd02d2
ellie timoney <flrl@users.noreply.github.com>
2025-08-28 11:29:59 +1000

    flrl: MALLOC_FAILED is unlikely, so hint that

--- flrl/flrl.h
+++ flrl/flrl.h
@@ -1,6 +1,20 @@
 #ifndef LIBFLRL_FLRL_H
 #define LIBFLRL_FLRL_H
 
-#define MALLOC_FAILED(x) (x)
+#ifndef __has_builtin
+# define __has_builtin(x) (0)
+#endif
+
+/* suppresses branch coverage (see lcovrc), and hints that the branch is
+ * unlikely. usage:
+ *
+ * p = malloc(n);
+ * if (MALLOC_FAILED(!p)) return -1;
+ */
+#if __has_builtin(__builtin_expect)
+# define MALLOC_FAILED(x) __builtin_expect((x), 0)
+#else
+# define MALLOC_FAILED(x) (x)
+#endif
 
 #endif
